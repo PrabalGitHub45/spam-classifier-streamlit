0d4e32b23e35d4960020b74666ff7c039b554497
Prabal Adhikari <93089857+PrabalGitHub45@users.noreply.github.com>
2025-04-14 12:46:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import os
 
+# Styling the page
+st.set_page_config(page_title="SMS Spam Classifier", page_icon="📩", layout="centered")
+
+# Download NLTK resources
 ps = PorterStemmer()
 nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
 if not os.path.exists(nltk_data_dir):
@@ -13,56 +17,40 @@ if not os.path.exists(nltk_data_dir):
 
 nltk.download('punkt_tab', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
-
 nltk.data.path.append(nltk_data_dir)
 
-
-# text preprocessing function
+# Preprocessing function
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-    #include stopwords
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-    # used stemming using porter stemmer
-    for i in text:
-        y.append(ps.stem(i))
+    y = [i for i in text if i.isalnum()]
+    y = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
+    y = [ps.stem(i) for i in y]
 
     return " ".join(y)
-# importing both the pickle files that we used to train model and vectorizer as tfidf and naive bayes
 
-tfidf = pickle.load(open('vectorizer1.pkl','rb'))
-mnb = pickle.load(open('model1.pkl','rb'))
+# Load model and vectorizer
+tfidf = pickle.load(open('vectorizer1.pkl', 'rb'))
+mnb = pickle.load(open('model1.pkl', 'rb'))
 
-#title of the app
-st.title("Email/SMS Spam Classifier")
+# App UI design
+st.markdown("<h1 style='text-align: center; color: #4B8BBE;'>📨 Email/SMS Spam Classifier</h1>", unsafe_allow_html=True)
+st.markdown("#### Paste any message below to check if it's **SPAM** or **NOT SPAM** 👇")
 
-#input area
-input_sms = st.text_area("Enter the message")
+input_sms = st.text_area("🔤 Enter the message here:", height=150)
 
-#predict button
-if st.button('Predict'):
-
-    # preprocess the sms
+# Button and result display
+if st.button('🚀 Predict', use_container_width=True):
     transformed_sms = transform_text(input_sms)
-    # vectorize the input
     vector_input = tfidf.transform([transformed_sms])
-    # predict
     result = mnb.predict(vector_input)[0]
-    
+
     if result == 1:
-        st.header("Spam")
+        st.error("🚫 This message is classified as **SPAM**.")
     else:
-        st.header("Not Spam")
+        st.success("✅ This message is classified as **NOT SPAM**.")
+
+# Footer
+st.markdown("---")
+st.markdown("<p style='text-align:center;'>Made with 💻 by <b>Prabal</b></p>", unsafe_allow_html=True)
